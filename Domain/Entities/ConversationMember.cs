ea928a67ecf17d438d01647671e0bcf41c56a092
Julian Homez <julianhomesdev@icloud.com>
2025-08-28 23:27:02 -0500

    New entities for chat logic created

---
+++ Domain/Entities/ConversationMember.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChatAll.Domain.Entities
+{
+    public class ConversationMember
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        public int ConversationId { get; set; }
+
+        // Relation with Conversation
+        [ForeignKey(nameof(ConversationId))]
+        public Conversation Conversation { get; set; } = null!;
+
+        // Relation with user
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; } = null!;
+
+
+
+
+
+    }
+}
